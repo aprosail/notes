3eec5ba077a389d87de32f1052d0efccc79b9a95
James Aprosail <aprosail@outlook.com>
2024-06-01 20:00:34 +0800

    basic frame

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,23 +1,65 @@
 import {defineConfig} from "vitepress"
+import {icons} from "./icons"
 
 export default defineConfig({
-  title: "Shared notes",
-  description: "Shared online notebook.",
+  title: "笔记分享",
+  description: "零基础跨平台应用开发系列",
   base: "/notes",
   themeConfig: {
+    logo: "/avatar.png",
     nav: [
-      {text: "Home", link: "/"},
-      {text: "Examples", link: "/markdown-examples"},
+      {text: "首页", link: "/"},
+      {text: "通用基础", link: "/docs/preface.md"},
+      {text: "开源社区", link: "/docs/open.md"},
+      {text: "Node", link: "/docs/node.md"},
+      {text: "Flutter", link: "/docs/flutter.md"},
+      {text: "Go", link: "/docs/go.md"},
+    ],
+    socialLinks: [
+      {
+        icon: {svg: icons.bilibili},
+        link: "https://space.bilibili.com/1030736671",
+      },
+      {icon: "github", link: "https://github.com/aprosail/notes"},
     ],
     sidebar: [
       {
-        text: "Examples",
-        items: [
-          {text: "Markdown Examples", link: "/markdown-examples"},
-          {text: "Runtime API Examples", link: "/api-examples"},
-        ],
+        text: "序言部分",
+        link: "/docs/preface.md",
+        // items: [],
+      },
+      {
+        text: "代码编辑器",
+        link: "/docs/editor.md",
+      },
+      {
+        text: "版本控制系统",
+        link: "/docs/vcs.md",
+      },
+      {
+        text: "Node基础",
+        link: "/docs/node.md",
+      },
+      {
+        text: "网页开发",
+        link: "/docs/frontend.md",
+      },
+      {
+        text: "网页打包应用",
+        link: "/docs/electron.md",
+      },
+      {
+        text: "Flutter应用开发",
+        link: "/docs/flutter.md",
+      },
+      {
+        text: "Go编程语言基础",
+        link: "/docs/go.md",
+      },
+      {
+        text: "开源社区入门指南",
+        link: "/docs/open.md",
       },
     ],
-    socialLinks: [{icon: "github", link: "https://github.com/vuejs/vitepress"}],
   },
 })
--- index.md
+++ index.md
@@ -3,23 +3,25 @@
 layout: home
 
 hero:
-  name: "Shared notes"
-  text: "Shared online notebook"
-  tagline: My great project tagline
+  name: 笔记分享
+  tagline: 零基础跨平台应用开发系列
   actions:
     - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
+      text: 开始阅读
+      link: /docs/preface
     - theme: alt
-      text: API Examples
-      link: /api-examples
+      text: 观看视频
+      link: https://github.com/aprosail/notes
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 零基础/低门槛
+    details: 对每个步骤的原理都有精确讲解。unfinished.
+  - title: 开源免费
+    details: >
+      源代码托管于 GitHub，使用 CC0 知识共享协议，
+      任何人都可免费阅读和下载到本地。
+      页面由 VuePress 构建，可在本地自行搭建预览。
+  - title: 笔记检索
+    details: >
+      unfinished.
 ---
-
