6c844ac7d67f8f82f452f76991092b7d8cabac9f
James Aprosail <aprosail@outlook.com>
2024-06-02 14:04:20 +0800

    bilibili video player

---
+++ docs/editor.md
@@ -0,0 +1,4 @@
+<script setup lang="ts">import Bilibili from '../tools/Bilibili.vue';</script>
+<Bilibili vid="BV1jm411o7tq" />
+
+正文部分，未完待续
---
+++ tools/Bilibili.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup>
+  import {defineProps} from "vue"
+  const props = defineProps({vid: {type: String, required: true}})
+</script>
+
+<template>
+  <div class="area">
+    <div class="container">
+      <iframe
+        :src="`//player.bilibili.com/player.html?bvid=${vid}&autoplay=0`"
+        class="player"
+        frameborder="no"
+        allowfullscreen
+      ></iframe>
+    </div>
+    <div class="comments">
+      观看视频建议直接前往B站, 此处播放器仅供预览, 不支持字幕且清晰度较低
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  div.area {
+    margin-top: 16px;
+    margin-bottom: 50px;
+  }
+
+  div.area > div.container {
+    position: relative;
+    width: 100%;
+    padding-top: 56.25%;
+    overflow: hidden;
+    border-radius: 16.18px;
+  }
+
+  div.area > div.container > iframe.player {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  div.area > div.comments {
+    margin-top: 10px;
+    margin-left: 65px;
+    margin-right: 65px;
+    margin-bottom: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    opacity: 50%;
+  }
+</style>
