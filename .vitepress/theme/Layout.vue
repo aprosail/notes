52a97307246d31f08eb156f611e6abef00d3a30c
James Aprosail <aprosail@outlook.com>
2024-06-05 02:56:35 +0800

    zh unnecessary spaces when md render

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,5 +1,26 @@
 <script setup lang="ts">
   import DefaultTheme from "vitepress/theme"
+  import {onMounted} from "vue"
+  import {resolveChineseSpaces} from "./chinese"
+
+  onMounted(function renderChineseSpaces() {
+    /** All tags that should be rendered. */
+    const query = (function () {
+      const handler: string[] = []
+      for (const tag of ["p", "li"]) {
+        handler.push(`div.vp-doc ${tag}`)
+      }
+      return handler.join(",")
+    })()
+
+    document.querySelectorAll(query).forEach(function (element) {
+      element as HTMLParagraphElement
+      element.childNodes.forEach(function (node) {
+        if (!(node instanceof Text)) return
+        node.replaceWith(resolveChineseSpaces(node.textContent ?? ""))
+      })
+    })
+  })
 </script>
 
 <template>
